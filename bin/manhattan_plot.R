ed50dce10dde5cdd391126e5af11665ea03d543b
PhilPalmer <phil.palmer.email@gmail.com>
2020-04-21 10:15:23 +0100

    Added volcano & QQ plot to interactive html output

--- bin/manhattan_plot.R
+++ bin/manhattan_plot.R
@@ -5,12 +5,15 @@ library(qqman)
 library(plotly)
 library(tidyverse)
 library(CMplot)
+library(manhattanly)
 
 args = commandArgs(trailingOnly=TRUE)
 assoc_file <- args[1] # eg "out.assoc"
 annot_file <- args[2] # eg "annotation_GWAS_subset.csv"
 
-# qqman
+##########
+### qqman
+##########
 assoc <- read.table(assoc_file, quote="\"", comment.char="", header=T)
 sig = 5e-8 # significant threshold line
 sugg = 1e-6 # suggestive threshold line
@@ -35,11 +38,15 @@ print(manhattan(assoc,
                 annotatePval = sugg, annotateTop = FALSE,
                 main = "Manhattan Plot GWAS Results\nQQman Plot"))
 
+##############################
+### Plotly for Manhattan + QQ 
+##############################
+
 annotation <- read.csv(annot_file, stringsAsFactors = F)
 geno <- annotation %>% select(c(rsid, Gene, Function))  %>%
   mutate_at(vars(Function), list(~ifelse(.=="NA", "intergenic",.)))
 gwas_input <- read.table(assoc_file, h=T, stringsAsFactors = F) %>% left_join(., geno, by=c("SNP"="rsid")) %>% mutate_at(vars(CHR,BP, OR),list(~replace_na(.,"")))%>% mutate_at(vars(CHR,BP, P, OR), list(~as.numeric(.))) 
-# Plotly for Manhattan + QQ #####
+
 # Prepare the dataset
 gwas_plot <- gwas_input %>% 
   group_by(CHR) %>%    # Compute chromosome size
@@ -49,12 +56,20 @@ gwas_plot <- gwas_input %>%
   left_join(gwas_input, ., by=c("CHR"="CHR")) %>% # Add this info to the initial dataset
   arrange(CHR, BP) %>% # Add a cumulative position of each SNP
   mutate(BPcum=BP+tot) %>%
-  mutate_at(vars(Gene,Function),list(~replace_na(.,"")))
+  mutate_at(vars(Gene,Function),list(~replace_na(.,""))) %>%
+  mutate(volcano_threshold=case_when(
+    .$OR <= -3 | .$OR >= 3 ~ "0",
+    TRUE ~ "1",
+  ))
+
 # Prepare X axis
 axisdf <- gwas_plot %>% group_by(CHR) %>% summarize(center=( max(BPcum) + min(BPcum) ) / 2 )
+
 # Prepare text description for each SNP:
-gwas_plot$text <- paste("SNP: ", gwas_plot$SNP, "\nPosition: ", gwas_plot$BP, "\nChromosome: ", gwas_plot$CHR, "\nFunction: ", gwas_plot$Function,
-                  "\nMAF: ", gwas_plot$F_A, "\nGene: ", gwas_plot$Gene, "\nOdds Ratio: ", gwas_plot$OR, sep="")
+gwas_plot$text <- paste("SNP: ", gwas_plot$SNP, "\nPosition: ", gwas_plot$BP, "\nChromosome: ", gwas_plot$CHR, "\nREF: ", gwas_plot$A1, "\nALT: ", gwas_plot$A2,
+                        "\nFunction: ",gwas_plot$Function,"\nMAF: ", gwas_plot$F_A, sprintf("\nP-value: %2e", round(gwas_plot$P,3)),
+                        "\nGene: ",gwas_plot$Gene, "\nOdds Ratio: ", gwas_plot$OR, sep="")
+
 # Manhattan
 p <- ggplot(gwas_plot, aes(x=BPcum, y=-log10(P), text=text)) +
   geom_point( aes(color=as.factor(CHR)), alpha=0.8, size=1.3) +
@@ -70,33 +85,70 @@ p <- ggplot(gwas_plot, aes(x=BPcum, y=-log10(P), text=text)) +
     panel.grid.major.x = element_blank(),
     panel.grid.minor.x = element_blank()
   )
-plot <- ggplotly(p, tooltip="text")
-htmlwidgets::saveWidget(as.widget(plot), "multiqc_report.html")
+
+manhattan <- ggplotly(p, tooltip="text")
+
 # Volcano
-p1 <-ggplot(gwas_plot, aes(x=OR, y=-log10(P),text=text)) +
-  geom_point(alpha=0.8, size=1.3) +
+p1 <-ggplot(gwas_plot, aes(x=OR, y=-log10(P), text=text)) +
+  geom_point(aes(colour=volcano_threshold), alpha=0.8, size=1.3) +
   xlab("Odds Ratio") + 
   ylab("-log10 p-value") +
+  ggtitle("Volcano Plot") +
   geom_vline(xintercept = 1, linetype="dotted", 
-             color = "blue", size=1.5)+
-  scale_color_manual(values = rep(c("grey", "skyblue"), 22 )) +
+             color = "skyblue", size=1)+
+  geom_vline(xintercept = 3, linetype="dotted", 
+             color = "skyblue", size=0.5)+
+  scale_color_manual(values = rep(c("red", "skyblue"), 22 )) +
   theme_bw() +
   theme( 
     legend.position="none",
     panel.border = element_blank(),
   )
-ggplotly(p1, tooltip="text")
-ggsave('volcano.png')
-png('qq.png', width=700, height=800)
-qq(gwas_input$P)
-# Make circular plot
-CMplot(gwasResults, plot.type="c", r=1.6, cir.legend=TRUE,
-        outward=TRUE, cir.legend.col="black", cir.chr.h=.1 ,chr.den.col="orange", file="jpg",
-        memo="", dpi=300, chr.labels=seq(1,22))
-#number of hits 
-sprintf("Number of variants tested %i", nrow(gwas_input))
-sprintf("Number of variants at suggestive significance %i", gwas_input %>% filter(P < 1e-5) %>% with(nrow(.)))
-sprintf("Number of variants at suggestive significance %i", gwas_input %>% filter(P < 1e-8) %>% with(nrow(.)))
+
+volcano <- ggplotly(p1, tooltip="text")
+
+qqplot <- qqly(gwas_plot %>% filter(P > 0), main= "QQ plot", snp="SNP", gene = "Gene")
+
+#qqly(HapMap, snp = "SNP", gene = "GENE")
+
+# Annotation table
+top_markers <- gwas_plot %>% dplyr::arrange(P) %>% slice(1:500) %>% select(SNP)
+annotation_top <- annotation %>% select(
+  Location,rsid,Reference,Alternative,Gene,Function,SIFT.Effect,Polyphen2_HDIV.Effect,MutationTaster.Effect, CADD.Score,
+  gnomAD.AF.All, gnomAD.AF.NFE) %>% filter(rsid %in% top_markers$SNP)
+
+table <- plot_ly(
+  type = 'table',
+  header = list(
+    columnwidth = c(80,600),
+    values = c(names(annotation_top)),
+    align = c('left', rep('center', ncol(annotation_top))),
+    line = list(width = 1, color = 'black'),
+    fill = list(color = 'rgb(235, 100, 230)'),
+    font = list(family = "Arial", size = 14, color = "white")
+  ),
+  cells = list(
+    values = rbind(
+      rownames(annotation_top),
+      t(as.matrix(unname(annotation_top)))
+    ),
+    align = c('left', rep('center', ncol(annotation_top))),
+    line = list(color = "black", width = 1),
+    fill = list(color = c('rgb(235, 193, 238)', 'rgba(228, 222, 249, 0.65)')),
+    font = list(family = "Arial", size = 12, color = c("black")),
+    domain = list(x=c(0,0.5), y=c(0,1))
+  ))
+
+# Plotting 
+plotly::subplot(manhattan, volcano, qqplot, nrows = 3, margin = 0.1, titleX = T, titleY = T) %>%
+  layout(title = "Manhattan, Volcano and QQ plots")#, height=(700))
+
+main_plot <- plotly::subplot(manhattan, volcano, qqplot, nrows = 3, margin = 0.1, titleX = T, titleY = T, heights = c(0.4,0.3,0.2)) %>%
+  layout(title = "Manhattan, Volcano and QQ plots")#, height=(700))
+
+htmlwidgets::saveWidget(as.widget(main_plot), "multiqc_report.html")
+htmlwidgets::saveWidget(as.widget(table), "annotated_table.html")
+
 # Table annotated to display
 write.table(annotation,  
             file      = 'annotation.csv',
@@ -105,4 +157,13 @@ write.table(annotation,
             sep       = ",",
             row.names = F,
             col.names = T)
-dev.off()
+
+# Make circular plot
+CMplot(gwasResults, plot.type="c", r=1.6, cir.legend=TRUE,
+        outward=TRUE, cir.legend.col="black", cir.chr.h=.1 ,chr.den.col="orange", file="jpg",
+        memo="", dpi=300, chr.labels=seq(1,22))
+
+#number of hits 
+sprintf("Number of variants tested %i", nrow(gwas_input))
+sprintf("Number of variants at suggestive significance %i", gwas_input %>% filter(P < 1e-5) %>% with(nrow(.)))
+sprintf("Number of variants at suggestive significance %i", gwas_input %>% filter(P < 1e-8) %>% with(nrow(.)))
